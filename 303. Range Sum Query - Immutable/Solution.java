8f74db1d13610b85529ebbce86bba48e20d5678d
kai <kaiion0509@gmail.com>
2021-08-16 19:57:09 -0400

    303. presum solution
    Faster approach than naive solution

--- 303. Range Sum Query - Immutable/Solution.java
+++ 303. Range Sum Query - Immutable/Solution.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class Solution {
-    static class NumArray {
+    /**
+     * Naive solution
+     */
+    /*static class NumArray {
         List<Integer> list = new ArrayList<>();
        public NumArray(int[] nums) {
            for(int i=0;i<nums.length;i++)
@@ -13,7 +16,28 @@ public class Solution {
            }
            return sum;
        }
+   }*/
+
+   /**
+    * Prefix solution
+    precalculate sums when first declaring array, then subtract left and right pointers
+    Time:
+        Constructor: O(N)
+        sumRange(left, right): O(1)
+    Space: O(1)
+    */
+   static class NumArray {
+    int[] preSum;
+   public NumArray(int[] nums) {
+       preSum = nums;
+       for(int i = 1;i < nums.length;i++)
+           preSum[i] = preSum[i-1] + nums[i];
    }
+   public int sumRange(int left, int right) {
+       if (left == 0) return preSum[right];
+       return preSum[right]-preSum[left-1];
+   }
+}
 
    //driver
    public static void main(String[] args) {
